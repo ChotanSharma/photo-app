8c877c4e7699ae9c030cd6a069b4e1b506b585f4
ChotanSharma <chotansharma@yahoo.com>
2022-10-28 10:33:41 -0400

    add useState and useEffect

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,6 +7,9 @@ const About = () => {
         <section className="my-5">
             <h1 id="about">Who am I?</h1>
             <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </section>
     )
 }   
